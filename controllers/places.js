0e8b18dc464d6b933c440708ff42743a50ff4254
unknown <danieleastwood4@gmail.com>
2022-03-30 21:28:15 -0400

    Delete implemented

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,14 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id] })
-  }
-})
+router.get('/', (req, res) => {
+  res.render('places/index', {places})
+  })
 
 router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+  res.render('places/new')
+})
+
 
   router.post('/', (req, res) => {
     console.log(req.body)
@@ -33,28 +25,31 @@ router.get('/new', (req, res) => {
     res.redirect('/places')
   })
 
-  router.get('/', (req, res) => {
-    let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'https://placekitten.com/250/250'
-    }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/cat1.jpg'
-    }, {
-    name: 'Placeholder',
-    city: 'Placeholder',
-    state: 'Placeholder',
-    cuisines: 'Placeholder',
-    pic: '/images/food1.png'
-    }]
-    res.render('places/index', {places})
-    })
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id], id })
+    }
+  })
 
+  router.delete('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(req.params.id, 1)
+      res.status(303).redirect('/places')
+    }
+  })
 
 module.exports = router
