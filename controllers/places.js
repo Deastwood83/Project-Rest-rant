54e2b9f2a4042654c03bd84a406231c28eb7dee9
unknown <danieleastwood4@gmail.com>
2022-04-01 19:32:15 -0400

    added edit form

--- controllers/places.js
+++ controllers/places.js
@@ -52,4 +52,18 @@ router.get('/new', (req, res) => {
     }
   })
 
-module.exports = router
+  router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+
+
+  module.exports = router
