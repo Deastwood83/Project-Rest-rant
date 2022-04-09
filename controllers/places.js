6fd984eb2fdfde722b14d10ef07795e38ccc31c7
unknown <danieleastwood4@gmail.com>
2022-04-08 20:25:31 -0400

    comment added

--- controllers/places.js
+++ controllers/places.js
@@ -30,7 +30,9 @@ router.get('/new', (req, res) => {
 
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-    .then(place =>{
+  .populate('comments')
+    .then(place => {
+      console.log(place.comments)
       res.render('places/show',{ place })
     })
     .catch(err =>{
